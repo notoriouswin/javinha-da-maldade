4409137ddec61ce33a561cbec85ec5ae6ed75557
notoriouswin <bessadev13@gmail.com>
2026-05-19 05:07:16 -0300

    Aprendendo sobre Foreach, e um metodo para simplicar a execução de todos os os valores dentro do array

---
+++ src/academy/devdojo/maratonajava/introducao/Aula17ArraysForeach.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula17ArraysForeach {
+    public static void main(String[]argas){
+        // Foreach é uma forma de simplificar o for, ele usa um sistema em que ele executa os valores do array em sequência sem ter que criar condições/indices com o for
+        String [] sobrenomes = {"Bessa","Silva","Joaquim","Queiroz"};
+        for (String i : sobrenomes){
+            System.out.println(i);
+        }
+        // só temos que lembrar que o tipo da variável dentro do for tem que ser igual ao do array
+    }
+}
