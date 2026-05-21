f0ba33f1bc9c3a26780e8d8cdf7153a2bc23ec16
notoriouswin <bessadev13@gmail.com>
2026-05-21 01:32:35 -0300

    Aprendendo sobre arrats multidimensionais

---
+++ src/academy/devdojo/maratonajava/introducao/Aula18ArraysMultidimensionais.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula18ArraysMultidimensionais {
+    public static void main(String[]args) {
+        int[][] dias = new int[3][3];
+
+        dias[0][0] = 1;
+        dias[0][1] = 2;
+        dias[0][2] = 3;
+
+        dias[1][0] = 4;
+        dias[1][1] = 5;
+        dias[1][2] = 6;
+
+        dias[2][0] = 7;
+        dias[2][1] = 8;
+        dias[2][2] = 9;
+
+        for (int i = 0; i < dias.length; i++){
+            for (int j = 0; j < dias[0].length; j++){
+                System.out.println(dias[i][j]);
+            }
+        }
+    }
+}
