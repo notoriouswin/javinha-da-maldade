2a7406e685d98ed319b3d2b61a30d33010efb1e9
notoriouswin <bessadev13@gmail.com>
2026-06-04 16:59:17 -0300

    arrays multidimencionais inicializações

--- src/academy/devdojo/maratonajava/introducao/Aula20ArraysMultidimensionaisInicializacao.java
+++ src/academy/devdojo/maratonajava/introducao/Aula20ArraysMultidimensionaisInicializacao.java
@@ -3,11 +3,12 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula20ArraysMultidimensionaisInicializacao {
     public static void main(String[]args){
         int[][] sistemaDeNotas = new int[3][];
-        sistemaDeNotas[0] = new int[2];
-        sistemaDeNotas[1] = new int[5];
-        sistemaDeNotas[2] = new int[8];
+        sistemaDeNotas[0] = new int [2];
+        sistemaDeNotas[1] = new int[4];
+        sistemaDeNotas[2] = new int[6];
 
         for (int[] i : sistemaDeNotas){
+            System.out.println("-----------");
             for (int j : i){
                 System.out.println(j);
             }
