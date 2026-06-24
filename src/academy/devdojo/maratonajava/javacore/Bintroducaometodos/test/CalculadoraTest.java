17db18addf05e1cb73207f9ff3faa5af63fff6ee
notoriouswin <bessadev13@gmail.com>
2026-06-23 22:48:49 -0300

    update

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest.java
@@ -3,7 +3,7 @@ package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
 import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
 
 public class CalculadoraTest {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
 
         calculadora.somaDoisNumeros();
@@ -12,20 +12,20 @@ public class CalculadoraTest {
         calculadora.subtraiDoisNumeros();
         System.out.println("Fim do metodo subtraiDoisNumeros \n");
 
-        calculadora.multiplicaDoisNumeros(10,5);
+        calculadora.multiplicaDoisNumeros(10, 5);
         System.out.println("Fim do metodo multiplicaDoisNumeros \n");
 
-        double result = calculadora.divideDoisNumeros(50,4);
+        double result = calculadora.divideDoisNumeros(50, 4);
         System.out.println(result);
 
         // alteraDoisNumeros EM AÇÃO
         int num1 = 1;
         int num2 = 2;
-        calculadora.alteraDoisNumeros(num1,num2);
+        calculadora.alteraDoisNumeros(num1, num2);
         System.out.println("-------");
         System.out.println("Dentro do Calculadora Test");
-        System.out.println("num1: "+num1);
-        System.out.println("num2: "+num2);
+        System.out.println("num1: " + num1);
+        System.out.println("num2: " + num2);
 
         /*
             a = 1 e b = 2 são copiados para numero1 e numero2.
@@ -39,5 +39,10 @@ public class CalculadoraTest {
             O método rabisca as cópias e escreve 99 e 33.
             Mas as folhas originais (a e b) ainda estão intocadas com 1 e 2.
          */
+
+        int []numeros = {1,2,3,4,5};
+        calculadora.somaArray(numeros);
+        calculadora.somaVarArgs(5,5,5,5,10,20);
+
     }
 }
