90e37ed52c69f93beca5d66242ae463d4c41c9f2
notoriouswin <bessadev13@gmail.com>
2026-07-16 03:29:22 -0300

    Entendendo função Math() em java

---
+++ nelio-alves/src/Math/MathApp.java
@@ -0,0 +1,26 @@
+package Math;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MathApp {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        Scanner scan = new Scanner(System.in);
+        MathFuncoes matematica = new MathFuncoes();
+
+        System.out.println("Escolha um numero");
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+        System.out.println("Escolha um numero negativo");
+        int z = scan.nextInt();
+        System.out.println("Escolha um numero decimal");
+        double w = scan.nextDouble();
+
+        matematica.raizQuadrada(x);
+        matematica.elevadoAoQuadrado(x,y);
+        matematica.valorAbsoluto(z);
+        matematica.arredondarNumero(w);
+    }
+}
---
+++ nelio-alves/src/Math/MathFuncoes.java
@@ -0,0 +1,28 @@
+package Math;
+
+public class MathFuncoes{
+
+    public void raizQuadrada(int x){
+        double resultado = Math.sqrt(x);
+        System.out.printf("A raiz quadrada de %d é: %.2f %n",x,resultado);
+        System.out.println("——————————————————————————————");
+    }
+
+    public void elevadoAoQuadrado(int x,int y){
+        double resultado = Math.pow(x,y);
+        System.out.printf("%d elevado a %d é %.2f %n", x,y,resultado);
+        System.out.println("——————————————————————————————");
+    }
+
+    public void valorAbsoluto(int x){
+        int resultado = Math.abs(x);
+        System.out.printf("O valor absoluto de %d é %d %n", x,resultado);
+        System.out.println("——————————————————————————————");
+    }
+
+    public void arredondarNumero(double x){
+        double resultado = (int)Math.round(x);
+        System.out.printf("O valor arredondado de %s é %s %n",x,resultado);
+        System.out.println("——————————————————————————————");
+    }
+}
